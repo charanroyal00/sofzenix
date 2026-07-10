90725f1277015d74fc88fad3442d0d6293fa6abf
Priyanshu Patil <priyanshu.patil.batch2024@sitnagur.siu.edu.in>
2026-07-10 22:26:21 +0530

    Fix Google Maps iframe embed with official pb token to prevent frame blocking

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -547,7 +547,7 @@ const Contact = () => {
             <div 
               className="w-full h-full"
               dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://maps.google.com/maps?q=Sofzenix%20IT%20Solutions%20LLP,%20Narasaraopeta&t=&z=15&ie=UTF8&iwloc=&output=embed" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+                __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15289.789182601725!2d80.0435889!3d16.2358475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4a753e1a067ff3%3A0xe10ad5be4f29a00b!2sNarasaraopeta%2C%20Andhra%20Pradesh%20522601!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
               }}
             />
           </div>
